bd948196f45985da617f8583dec46f2a8a339da4
rhancock <rhancock@rhancock-X201.(none)>
2011-09-26 11:22:24 -0400

    version 1.1

--- get_meetup_rsvp.py
+++ get_meetup_rsvp.py
@@ -1,15 +1,18 @@
 """
 Retrieve the "yes" rsvps for a specific Meetup id.
 Get first and last name.
+Filter out last names that are only one letter.
 Write to a Google Docs spreadsheet.
 """
 import os.path
 import sys
 import json
 import operator
-import tempfile
 import time
 import string
+import pprint
+import optparse
+import codecs
 
 try:
     import settings
@@ -23,7 +26,7 @@ try:
     import gdata.docs.client
     import gdata.docs.data
     import gdata.acl.data
-except ImportError:
+except ImportError as e:
     sys.stderr.write("Failed to import the gdata client library: {ex}".format(ex=e))
     sys.exit(1)
 
@@ -34,7 +37,7 @@ except ImportError as e:
     sys.exit(1)
     
 __author__ = "hancock.robert@gmail.com"
-__version__ = "1.0"
+__version__ = "1.1"
 
 class HTTPError(Exception):
     def __init__(self, value):
@@ -82,15 +85,15 @@ class RSVP():
         event_id   The unique id that Meetup uses to identify this event.
     
     """
-    def __init__(self, event_id):
+    def __init__(self, event_id, filternames=True):
         self.event_id = event_id
         self.json_rsvps = {}
         self.names = []
-        self.tempfile = ""
-        # A translation table to filter out punctuation characters.
-        self.trans = string.maketrans(string.punctuation, " "*len(string.punctuation))
+        self.tempfile = None
+        self.tempfile_name = os.path.join(os.getcwd(), str(int(time.time()))) + ".csv"
+        self.filternames = filternames
+        self.trans = list(string.punctuation) # for unicode cleaning
         
-
     def download(self):
         """
         Get the "YES" RSVPs for this event.
@@ -127,56 +130,87 @@ class RSVP():
         
         [Bilbo] [Baggins, Esq.]
         [Zeljko] [Miscosich Rentlanich]
+        
+        Encode everything to UTF-8.
         """
         results = self.json_rsvps["results"]
         
         for line in results:
+            # For debugging: it "pretty prints" the dictonary to stdout
+            #if line["member"]:
+                #pprint.pprint(line)
+                #print("-"*75)
+                
             if line["response"] != 'yes':
                 continue
             
-            name = str(line["member"]["name"])
-            name = name.translate(self.trans)
-                
-            name_components = name.strip().split()
-            if not name_components:
-                continue
-            
-            l = len(name_components)
-            fname = lname = ""
-            
-            fname = name_components[0]
-            fname = fname[0].upper()+fname[1:]
-            if l == 2:
-                lname = name_components[1]
-            elif l > 2:
-                lname = " ".join(name_components[2:])
-            
-            if lname:
-                # The last name cannot be one letter or one letter plus a period
-                lname = lname[0].upper()+lname[1:]
-                if len(lname) ==1:
+            name = unicode(line["member"]["name"])
+            name = self._clean_unicode(name)
+            if self.filternames:
+                if len(name) < 4:
+                    sys.stderr.write("Removed: '{f}' - invalid full name.\n".format(f=name))
                     continue
                 
-                if len(lname) ==2:
-                    if lname[1] == ".":
+                name_components = name.strip().strip(',').split()
+                if not name_components:
+                    continue
+                
+                if len(name_components) < 2:
+                    sys.stderr.write("Removed: '{f}' - invalid full name.\n".format(f=name))
+                    continue
+                                
+                fname = lname = ""
+                
+                fname = name_components[0]
+                if len(fname) == 1:
+                    sys.stderr.write("Removed: '{f}' - first name cannot be one letter.\n".format(f=name))
+                    continue
+                
+                fname = fname[0].upper()+fname[1:]
+                #lname = " ".join(name_components[2:])
+                lname = " ".join(name_components[1:])
+                
+                if lname:
+                    # The last name cannot be one letter.
+                    lname = lname[0].upper()+lname[1:]
+                    if len(lname) == 1:
+                        sys.stderr.write("Removed: '{l}' last name cannot be one letter.\n".format(l=name))
                         continue
                     
-
-            self.names.append((fname,lname))
+                self.names.append((fname,lname))
+            else:
+                self.names.append((name,""))
             
-        self.names = sorted(self.names, key=operator.itemgetter(1))
+        if self.filternames:
+            self.names = sorted(self.names, key=operator.itemgetter(1))
+        else:
+            self.names.sort()
     
-        
+    
     def write_to_file(self):
         """ Write the list of names to a CSV file. """
-        f = tempfile.NamedTemporaryFile(delete=False, suffix="csv")
-        self.tempfile = f.name
+
+        self.tempfile = codecs.open(self.tempfile_name, mode="w", encoding="utf-8")
         
         for entry in self.names:
             fname, lname = entry
-            f.write("{f},{l}\n".format(f=fname, l=lname))
-        f.close()
+
+            self.tempfile.write(fname+","+lname+"\n")
         
+        self.tempfile.close()
+        
+        
+    def _clean_unicode(self, s):
+        """ A naive way to clean punctuations chars from the unicode string. """
+        for c in s:
+            if c in self.trans:
+                i = s.find(c)
+                if i == len(s) - 1:
+                    s = s[:1]
+                else:
+                    s = s[:i] + s[i+1:]
+        return s
+ 
         
 class Spreadsheet():
     """ Upload CSV file to a Google Docs spreadsheet. 
@@ -195,11 +229,8 @@ class Spreadsheet():
         self.client = gdata.docs.client.DocsClient(source=self.title)
         self.client.ClientLogin(settings.EMAIL, settings.PASSWORD, self.client.source)
         
-        try:
-            self.entry = self.client.Upload(self.fil, self.title, content_type='text/csv')        
-        except Exception as e:
-            print(e)
-            return
+        # If this fails, let the exception bubble up
+        self.entry = self.client.Upload(self.fil, self.title, content_type='text/csv')        
         
 
     def share(self):
@@ -212,22 +243,63 @@ class Spreadsheet():
             new_acl = self.client.Post(acl_entry, self.entry.GetAclFeedLink().href)
             #print "%s %s added as a %s" % (new_acl.scope.type, new_acl.scope.value, new_acl.role.value)
     
-def main():
-    m = MeetupEvent(settings.GROUP_URLNAME)
-    event_id = m.get_next_event()
     
-    rsvp = RSVP(event_id)
-    rsvp.download()
+def main():
+    usage = "%prog [-inv]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-i", "--event-id",
+                      action="store", type="string", dest="eventid",
+                      help="Use this event id and do not attempt to find the next one automatically on Meetup.")
+    parser.add_option("-v", "--version",
+                      action="store_true", dest="version",
+                      help="Print version.")
+    parser.add_option("-n", "--skip-name-filtering",
+                      action="store_true", dest="skip_name_filtering",
+                      help="Ignore name filtering rules.")
+    
+    [options, args] = parser.parse_args()
+    if options.version:
+        print("Version: {v}".format(v=__version__))
+        return 0
+    
+    if not options.eventid:
+        m = MeetupEvent(settings.GROUP_URLNAME)
+        event_id = m.get_next_event()
+    else:
+        event_id = options.eventid
+        
+    filter_names = False if options.skip_name_filtering else True
+    rsvp = RSVP(event_id, filternames=filter_names)
+    try:
+        rsvp.download()
+    except Exception as e:
+        sys.stderr.write("Cannot download event {i}: {e}".format(i=event_id, e=e))
+        sys.exit(-1)
+        
     rsvp.get_names()
+    if not rsvp.names:
+        sys.stderr.write("No names were found for event id {ei}.".format(ei=event_id))
+        sys.exti()
+        
+    #print("{n} names".format(n=len(rsvp.names)))
     rsvp.write_to_file()
     
     title = "{g}-{id}-{t}".format(g=settings.GROUP_URLNAME, 
                                   id=event_id, t=str(time.time()))
-    s = Spreadsheet(title, rsvp.tempfile)
-    s.upload()
-    s.share()
+    s = Spreadsheet(title, rsvp.tempfile_name)
+    try:
+        s.upload()       
+    except Exception as e:
+        sys.stderr.write("Could not create Google docs spreadsheet: {e}".format(e=e))
+        sys.exit()
+
+    try:
+        s.share()
+    except Exception as e:
+        sys.stderr.write("Could not share spreadsheet: {e}".format(e=e))
+        sys.exit()
     
-    os.remove(rsvp.tempfile)
+    os.remove(rsvp.tempfile_name)
     
 if __name__ == "__main__":
     main()
